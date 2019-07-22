9ccf3bceb6c31f68764d90fe7c1ede521f655c7f
Mario Ruiz <mjruiz@fb.com>
2019-07-22 10:25:47 -0700

    added location manager class to handle location stuff

---
+++ app/src/main/java/com/example/curate/utils/LocationManager.java
@@ -0,0 +1,78 @@
+package com.example.curate.utils;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.parse.ParseGeoPoint;
+
+public class LocationManager {
+    public static final int PERMISSION_REQUEST_CODE = 7;
+    private static final String[] LOCATION_PERMISSION = {Manifest.permission.ACCESS_FINE_LOCATION};
+
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Context mContext;
+
+    public LocationManager(Context context) {
+        mContext = context;
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(mContext);
+        requestPermissionsIfNecessary();
+    }
+
+    private void requestPermissionsIfNecessary() {
+        if (ContextCompat.checkSelfPermission(mContext, LOCATION_PERMISSION[0])
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions((Activity) mContext,
+                    LOCATION_PERMISSION, PERMISSION_REQUEST_CODE);
+        }
+
+    }
+
+    public void getCurrentLocation(OnSuccessListener<Location> callback) throws PermissionError {
+        if (ContextCompat.checkSelfPermission(mContext, LOCATION_PERMISSION[0])
+                != PackageManager.PERMISSION_GRANTED) {
+            throw new PermissionError();
+        }
+        mFusedLocationClient.getLastLocation().addOnSuccessListener(callback);
+    }
+
+    public void registerLocationUpdateCallback(LocationCallback callback) throws PermissionError {
+        if (ContextCompat.checkSelfPermission(mContext, LOCATION_PERMISSION[0])
+                != PackageManager.PERMISSION_GRANTED) {
+            throw new PermissionError();
+        }
+
+        // Create a request type that balances power and accuracy
+        LocationRequest request = new LocationRequest();
+        request.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+        request.setFastestInterval(60000L);
+        request.setInterval(3600000L);
+
+        mFusedLocationClient.requestLocationUpdates(request, callback, mContext.getMainLooper());
+    }
+
+    public void deregisterLocationUpdateCallback(LocationCallback callback) {
+        mFusedLocationClient.removeLocationUpdates(callback);
+    }
+
+    public static ParseGeoPoint createGeoPointFromLocation(Location location) {
+        ParseGeoPoint parseLocation = new ParseGeoPoint();
+        parseLocation.setLatitude(location.getLatitude());
+        parseLocation.setLongitude(location.getLongitude());
+        return parseLocation;
+    }
+
+    public static class PermissionError extends Error {
+
+    }
+}
