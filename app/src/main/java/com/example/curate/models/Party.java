80ccddbead27b04a687b9c12f6c06d78b804e4e7
SuperMario210 <Sup3rMar1o210@gmail.com>
2019-07-29 16:34:06 -0700

    moved playlist methods over to the playlist class

--- app/src/main/java/com/example/curate/models/Party.java
+++ app/src/main/java/com/example/curate/models/Party.java
@@ -22,26 +22,26 @@ import java.util.List;
 import java.util.Objects;
 
 @ParseClassName("Party")
-public class Party extends ParseObject {
+public class Party extends ComparableParseObject {
     private static final String ADMIN_KEY = "admin";
     private static final String JOIN_CODE_KEY = "joinCode";
     private static final String CURRENTLY_PLAYING_KEY = "currPlaying";
     private static final String NAME_KEY = "name";
     private static final String LOCATION_KEY = "location";
     private static final String LOCATION_PERMISSION_KEY = "locationEnabled";
+    private static final String CACHED_PLAYLIST_KEY = "cachedPlaylist";
 
     private static Party mCurrentParty;
-    private List<PlaylistEntry> mPlaylist;
+    private Playlist mPlaylist;
     private List<SaveCallback> mPlaylistUpdateCallbacks;
 
     public Party() {}
-
 
     /**
      * Initializes the party object and sets up live queries
      */
     private void initialize() {
-        mPlaylist = new ArrayList<>();
+        mPlaylist = new Playlist();
         mPlaylistUpdateCallbacks = new ArrayList<>();
 
         // Set up live query
@@ -57,11 +57,12 @@ public class Party extends ParseObject {
             if(currentlyPlaying != null) {
                 mCurrentParty.put(CURRENTLY_PLAYING_KEY, currentlyPlaying);
             }
-            updatePlaylist(e -> {
-                for(SaveCallback callback : mPlaylistUpdateCallbacks) {
-                    callback.done(e);
-                }
-            });
+
+            mPlaylist.update(party.getString(CACHED_PLAYLIST_KEY));
+
+            for(SaveCallback callback : mPlaylistUpdateCallbacks) {
+                callback.done(null);
+            }
         });
     }
 
@@ -353,7 +354,14 @@ public class Party extends ParseObject {
     }
 
 
-    // Playlist update methods
+    /**
+     * Gets the party's playlist.  Make sure to call updatePlaylist() before getting this value for
+     * the first time otherwise it will be empty.
+     * @return a list of playlist entries representing this party's playlist
+     */
+    public Playlist getPlaylist() {
+        return mPlaylist;
+    }
 
     /**
      * Registers a new callback that is called when the party's playlist changes
@@ -369,183 +377,6 @@ public class Party extends ParseObject {
      */
     public void deregisterPlaylistUpdateCallback(SaveCallback callback) {
         mPlaylistUpdateCallbacks.remove(callback);
-    }
-
-    /**
-     * Updates the party's playlist.  The updated playlist can be accessed by calling getPlaylist()
-     * in the callback
-     * @param callback callback to run after the cloud function is executed
-     */
-    public void updatePlaylist(@Nullable final SaveCallback callback) {
-        HashMap<String, Object> params = new HashMap<>();
-
-        ParseCloud.callFunctionInBackground("getPlaylist", params, (List<PlaylistEntry> playlist, ParseException e) -> {
-            if (e == null) {
-                // Preserve the playlist object so that it can be used in an adapter
-                mPlaylist.clear();
-                mPlaylist.addAll(playlist);
-            } else {
-                // Log the error if we get one
-                Log.e("Party.java", "Could not get playlist!", e);
-            }
-
-            // Run the callback if it exists
-            if(callback != null) {
-                callback.done(e);
-            }
-        });
-    }
-
-    /**
-     * Gets the party's playlist.  Make sure to call updatePlaylist() before getting this value for
-     * the first time otherwise it will be empty.
-     * @return a list of playlist entries representing this party's playlist
-     */
-    public List<PlaylistEntry> getPlaylist() {
-        return mPlaylist;
-    }
-
-
-    // Song methods
-
-    /**
-     * Likes a song in the party's playlist
-     * @param spotifyId the spotifyId of the song to like
-     * @param callback callback to run after the cloud function is executed
-     */
-    public void likeSong(String spotifyId, @Nullable final SaveCallback callback) {
-        HashMap<String, Object> params = new HashMap<>();
-        params.put(Song.SPOTIFY_ID_KEY, spotifyId);
-
-        ParseCloud.callFunctionInBackground("likeSong", params, (List<PlaylistEntry> playlist, ParseException e) -> {
-            if (e == null) {
-                // Preserve the playlist object so that it can be used in an adapter
-                mPlaylist.clear();
-                mPlaylist.addAll(playlist);
-            } else {
-                // Log the error if we get one
-                Log.e("Party.java", "Could not like song!", e);
-            }
-
-            // Run the callback if it exists
-            if(callback != null) {
-                callback.done(e);
-            }
-        });
-    }
-
-    /**
-     * Unlikes a song in the party's playlist
-     * @param spotifyId the spotifyId of the song to unlike
-     * @param callback callback to run after the cloud function is executed
-     */
-    public void unlikeSong(String spotifyId, @Nullable final SaveCallback callback) {
-        HashMap<String, Object> params = new HashMap<>();
-        params.put(Song.SPOTIFY_ID_KEY, spotifyId);
-
-        ParseCloud.callFunctionInBackground("unlikeSong", params, (List<PlaylistEntry> playlist, ParseException e) -> {
-            if (e == null) {
-                // Preserve the playlist object so that it can be used in an adapter
-                mPlaylist.clear();
-                mPlaylist.addAll(playlist);
-            } else {
-                // Log the error if we get one
-                Log.e("Party.java", "Could not unlike song!", e);
-            }
-
-            // Run the callback if it exists
-            if(callback != null) {
-                callback.done(e);
-            }
-        });
-    }
-
-    /**
-     * Adds a song to the party's playlist
-     * @param song the song to add to the playlist
-     * @param callback callback to run after the cloud function is executed
-     */
-    public void addSong(Song song, @Nullable final SaveCallback callback) {
-        HashMap<String, Object> params = new HashMap<>();
-        params.put(Song.SPOTIFY_ID_KEY, song.getSpotifyId());
-        params.put(Song.TITLE_KEY, song.getTitle());
-        params.put(Song.ARTIST_KEY, song.getArtist());
-        params.put(Song.ALBUM_KEY, song.getAlbum());
-        params.put(Song.IMAGE_URL_KEY, song.getImageUrl());
-
-        ParseCloud.callFunctionInBackground("addSong", params, (List<PlaylistEntry> playlist, ParseException e) -> {
-            if (e == null) {
-                // Preserve the playlist object so that it can be used in an adapter
-                mPlaylist.clear();
-                mPlaylist.addAll(playlist);
-            } else {
-                // Log the error if we get one
-                Log.e("Party.java", "Could not add song!", e);
-            }
-
-            // Run the callback if it exists
-            if(callback != null) {
-                callback.done(e);
-            }
-        });
-    }
-
-    /**
-     * Removes a song from the party's playlist
-     * @param song the song to remove from the playlist
-     * @param callback callback to run after the cloud function is executed
-     */
-    public void removeSong(Song song, @Nullable final SaveCallback callback) {
-        removeSong(song.getSpotifyId(), callback);
-    }
-
-    /**
-     * Removes a song from the party's playlist
-     * @param spotifyId the spotifyId of the song to remove from the playlist
-     * @param callback callback to run after the cloud function is executed
-     */
-    public void removeSong(String spotifyId, @Nullable final SaveCallback callback) {
-        HashMap<String, Object> params = new HashMap<>();
-        params.put(Song.SPOTIFY_ID_KEY, spotifyId);
-
-        ParseCloud.callFunctionInBackground("removeSong", params, (List<PlaylistEntry> playlist, ParseException e) -> {
-            if (e == null) {
-                // Preserve the playlist object so that it can be used in an adapter
-                mPlaylist.clear();
-                mPlaylist.addAll(playlist);
-            } else {
-                // Log the error if we get one
-                Log.e("Party.java", "Could not remove song!", e);
-            }
-
-            // Run the callback if it exists
-            if(callback != null) {
-                callback.done(e);
-            }
-        });
-    }
-
-    /**
-     * Calls on the ParseCloud to remove the next song in the party's playlist and return it
-     * @param callback callback to run after the cloud function is executed
-     * @return the party's currently playing song
-     */
-    public void getNextSong(@Nullable final SaveCallback callback) {
-        HashMap<String, Object> params = new HashMap<>();
-
-        ParseCloud.callFunctionInBackground("getNextSong", params, (FunctionCallback<Song>) (song, e) -> {
-            if (e == null) {
-                Log.d("Party.java", "got song " + song.getTitle());
-                mCurrentParty.put(CURRENTLY_PLAYING_KEY, (Song) Objects.requireNonNull(mCurrentParty.getParseObject(CURRENTLY_PLAYING_KEY)));
-            } else {
-                Log.e("Party.java", "Could not get the next song");
-            }
-
-            // Run the callback if it exists
-            if(callback != null) {
-                callback.done(e);
-            }
-        });
     }
 
     /**
@@ -597,20 +428,6 @@ public class Party extends ParseObject {
     }
 
     /***
-     * Checks if a song is already added to the playlist
-     * @param song song to check if added
-     * @return true if song is added, false if not
-     */
-    public boolean contains(Song song) {
-        // TODO: Improve efficiency
-        for(int i = 0; i < mPlaylist.size(); i++) {
-            if(mPlaylist.get(i).getSong().getSpotifyId().equals(song.getSpotifyId()))
-                return true;
-        }
-        return false;
-    }
-
-    /***
      * Saves all settings for the current party
      * @param locationEnabled whether location is enabled
      * @param partyName name for the party
@@ -634,5 +451,4 @@ public class Party extends ParseObject {
             }
         });
     }
-
 }
---
+++ app/src/main/java/com/example/curate/models/Playlist.java
@@ -0,0 +1,176 @@
+package com.example.curate.models;
+
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
+import com.parse.ParseCloud;
+import com.parse.ParseException;
+import com.parse.SaveCallback;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class Playlist {
+    private static final String ENTRY_ID = "entryId";
+
+    private List<PlaylistEntry> mSongs;
+    private List<Like> mLikes;
+
+    /***
+     * Checks if a song is already added to the playlist
+     * @param song song to check if added
+     * @return true if song is added, false if not
+     */
+    public boolean contains(Song song) {
+        // TODO: Improve efficiency
+        for(int i = 0; i < mSongs.size(); i++) {
+            if(mSongs.get(i).getSong().getSpotifyId().equals(song.getSpotifyId()))
+                return true;
+        }
+        return false;
+    }
+
+    /**
+     * Likes a song in the party's playlist
+     * @param entry the entry in the playlist to like
+     * @param callback callback to run after the cloud function is executed
+     */
+    public void likeEntry(PlaylistEntry entry, @Nullable final SaveCallback callback) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put(ENTRY_ID, entry.getObjectId());
+
+        ParseCloud.callFunctionInBackground("likeSong", params, (Like like, ParseException e) -> {
+            if (e == null) {
+                mLikes.add(like);
+            } else {
+                // Log the error if we get one
+                Log.e("Party.java", "Could not like song!", e);
+            }
+
+            // Run the callback if it exists
+            if(callback != null) {
+                callback.done(e);
+            }
+        });
+    }
+
+    /**
+     * Unlikes a song in the party's playlist
+     * @param entry the entry in the playlist to unlike
+     * @param callback callback to run after the cloud function is executed
+     */
+    public void unlikeEntry(PlaylistEntry entry, @Nullable final SaveCallback callback) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put(ENTRY_ID, entry.getObjectId());
+
+        ParseCloud.callFunctionInBackground("unlikeSong", params, (Like like, ParseException e) -> {
+            if (e == null) {
+                mLikes.remove(like);
+            } else {
+                // Log the error if we get one
+                Log.e("Party.java", "Could not unlike song!", e);
+            }
+
+            // Run the callback if it exists
+            if(callback != null) {
+                callback.done(e);
+            }
+        });
+    }
+
+    /**
+     * Adds a song to the party's playlist
+     * @param song the song to add to the playlist
+     * @param callback callback to run after the cloud function is executed
+     */
+    public void addEntry(Song song, @Nullable final SaveCallback callback) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put(Song.SPOTIFY_ID_KEY, song.getSpotifyId());
+        params.put(Song.TITLE_KEY, song.getTitle());
+        params.put(Song.ARTIST_KEY, song.getArtist());
+        params.put(Song.ALBUM_KEY, song.getAlbum());
+        params.put(Song.IMAGE_URL_KEY, song.getImageUrl());
+
+        ParseCloud.callFunctionInBackground("addSong", params, (List<PlaylistEntry> playlist, ParseException e) -> {
+            if (e == null) {
+                // Preserve the playlist object so that it can be used in an adapter
+                mSongs.clear();
+                mSongs.addAll(playlist);
+            } else {
+                // Log the error if we get one
+                Log.e("Party.java", "Could not add song!", e);
+            }
+
+            // Run the callback if it exists
+            if(callback != null) {
+                callback.done(e);
+            }
+        });
+    }
+
+    /**
+     * Removes a song from the party's playlist
+     * @param entry the entry to remove from the playlist
+     * @param callback callback to run after the cloud function is executed
+     */
+    public void removeEntry(PlaylistEntry entry, @Nullable final SaveCallback callback) {
+        HashMap<String, Object> params = new HashMap<>();
+        params.put(ENTRY_ID, entry.getObjectId());
+
+        ParseCloud.callFunctionInBackground("removeSong", params, (List<PlaylistEntry> playlist, ParseException e) -> {
+            if (e == null) {
+                // Preserve the playlist object so that it can be used in an adapter
+                mSongs.clear();
+                mSongs.addAll(playlist);
+            } else {
+                // Log the error if we get one
+                Log.e("Party.java", "Could not remove song!", e);
+            }
+
+            // Run the callback if it exists
+            if(callback != null) {
+                callback.done(e);
+            }
+        });
+    }
+
+    public List<PlaylistEntry> getEntries() {
+        return mSongs;
+    }
+
+    /**
+     * Updates the party's playlist from the parse server.  The updated playlist can be accessed by
+     * calling getEntries() in the callback.  This method should not be called unless it is
+     * absolutely necessary!
+     * @param callback callback to run after the cloud function is executed
+     */
+    public void update(@Nullable final SaveCallback callback) {
+        HashMap<String, Object> params = new HashMap<>();
+
+        ParseCloud.callFunctionInBackground("getPlaylist", params, (List<PlaylistEntry> playlist, ParseException e) -> {
+            if (e == null) {
+                // Preserve the playlist object so that it can be used in an adapter
+                mSongs.clear();
+                mSongs.addAll(playlist);
+            } else {
+                // Log the error if we get one
+                Log.e("Party.java", "Could not get playlist!", e);
+            }
+
+            // Run the callback if it exists
+            if(callback != null) {
+                callback.done(e);
+            }
+        });
+    }
+
+    public void update(String cachedPlaylist) {
+
+    }
+
+    public boolean isEmpty() {
+        return mSongs.isEmpty();
+    }
+
+}
