6b3e1489a4e3d4c1400d8c368240d2f68e32437a
Caio Costa <ccosta@princeton.edu>
2019-08-08 15:39:40 -0700

    Removed unused CustomBarcodeEncoder

--- app/src/main/java/com/example/curate/fragments/InfoDialogFragment.java
+++ app/src/main/java/com/example/curate/fragments/InfoDialogFragment.java
@@ -22,7 +22,6 @@ import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.example.curate.R;
 import com.example.curate.models.Party;
-import com.example.curate.utils.CustomBarcodeEncoder;
 import com.google.zxing.BarcodeFormat;
 import com.google.zxing.WriterException;
 import com.journeyapps.barcodescanner.BarcodeEncoder;
