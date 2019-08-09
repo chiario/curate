b74a6536a3d67c3a7248ac1487b8eb7c44a42da4
Caio Costa <ccosta@princeton.edu>
2019-08-09 15:53:52 -0700

    Changed notification icon and hide keyboard when submitting input

--- app/src/main/java/com/example/curate/fragments/BlurDialogFragment.java
+++ app/src/main/java/com/example/curate/fragments/BlurDialogFragment.java
@@ -3,7 +3,9 @@ package com.example.curate.fragments;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.animation.ValueAnimator;
+import android.content.Context;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 
 import androidx.fragment.app.Fragment;
 
@@ -42,6 +44,8 @@ public class BlurDialogFragment extends Fragment {
     }
 
     public void onHide(final Runnable onComplete) {
+        InputMethodManager imm = (InputMethodManager) getActivity().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.hideSoftInputFromWindow(getView().getWindowToken(), 0);
         mRoot.animate().alpha(0f).setDuration(500).setListener(new AnimatorListenerAdapter() {
             @Override
             public void onAnimationEnd(Animator animation) {
