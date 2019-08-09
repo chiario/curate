90200d31469ba4faf973b81cdd2d74a31adc1655
SuperMario210 <Sup3rMar1o210@gmail.com>
2019-08-09 12:12:13 -0700

    added dialog fragment class

---
+++ app/src/main/java/com/example/curate/fragments/DialogFragment.java
@@ -0,0 +1,57 @@
+package com.example.curate.fragments;
+
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ValueAnimator;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.curate.R;
+import com.github.mmin18.widget.RealtimeBlurView;
+
+public class DialogFragment extends Fragment {
+    View mRoot;
+    RealtimeBlurView mBlurView;
+
+    public View onShow(View root) {
+        mRoot = root;
+        mRoot.setAlpha(0f);
+
+        mBlurView = mRoot.findViewById(R.id.blurLayout);
+        mBlurView.setVisibility(View.GONE);
+
+        float blurRadius = getResources().getDimension(R.dimen.blur_radius);
+        ValueAnimator blurAnimator = ValueAnimator.ofFloat(0f, blurRadius);
+        blurAnimator.setDuration(250);
+        blurAnimator.addUpdateListener(animation -> {
+            float animatedValue = (float) animation.getAnimatedValue();
+            mBlurView.setBlurRadius(animatedValue);
+        });
+
+        mRoot.animate().alpha(1f).setDuration(250).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                mBlurView.setVisibility(View.VISIBLE);
+                blurAnimator.start();
+            }
+        });
+
+
+        return mRoot;
+    }
+
+    public void onHide(final Runnable onComplete) {
+        mRoot.animate().alpha(0f).setDuration(500).setListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                onComplete.run();
+            }
+        });
+    }
+}
